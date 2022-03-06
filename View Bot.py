e173853c529109129210ea2c710ecfa7d7091f7e
Quicksilver_YT <96615931+QuicksilverYT@users.noreply.github.com>
2022-03-06 15:57:50 +0530

    Add files via upload
    This is a view bot that helps you getting views on your youtube channel

---
+++ View Bot.py
@@ -0,0 +1,28 @@
+#Things you need
+import webbrowser, time
+import random
+from datetime import datetime
+current = datetime.now().strftime("%H:%M:%S")
+print("This started at -", current)
+print("  ")
+#Inputs
+url = input("Enter the URL of the video you want to get views ")
+
+
+
+#YT VIEW CODE
+for i in range(int(input("How much views do you want to get ?"))):
+    print("running video for {} times".format(i))  #This will show that how many times the video is running
+    webbrowser.open_new(url)
+    time.sleep(random.randint(3, 10))
+
+#last print
+print("  ")
+print("This ended at -", current)
+print("  ")
+time.sleep(2)
+print("Hope you found it useful ")
+print("  ")
+time.sleep(0.5)
+print("If you did not receive your views ,please wait for 10 to 20 minutes")
+
